8059c7cddd98860b3066a18f32b3f70067fe8c96
Martin Stofko <martinstofko114@gmail.com>
2018-08-05 20:05:38 -0700

    Built list of cats.

--- lib/ui/cat_list.dart
+++ lib/ui/cat_list.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'package:catbox/services/api.dart';
@@ -21,7 +22,68 @@ class _CatListState extends State<CatList> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink,
-      body: _getAppTitleWidget(),
+      body: _buildBody(),
+    );
+  }
+
+  Future<Null> refresh() {
+    _loadCats();
+    return Future<Null>.value();
+  }
+
+  Widget _buildBody() {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(8.0, 56.0, 8.0, 0.0),
+      child: Column(
+        children: <Widget>[
+          _getAppTitleWidget(),
+          _getListViewWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _getListViewWidget() {
+    return Flexible(
+      child: RefreshIndicator(
+        onRefresh: refresh,
+        child: ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          itemCount: _cats.length,
+          itemBuilder: _getListViewItem,
+        ),
+      ),
+    );
+  }
+
+  Widget _getListViewItem(BuildContext context, int index) {
+    Cat cat = _cats[index];
+
+    return Container(
+      margin: const EdgeInsets.only(top: 4.0),
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Hero(
+                tag: index,
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(cat.avatarUrl),
+                ),
+              ),
+              title: Text(
+                cat.name,
+                style: TextStyle(
+                    fontWeight: FontWeight.w700, color: Colors.black54),
+              ),
+              subtitle: Text(cat.description),
+              isThreeLine: true,
+              dense: false,
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -39,11 +101,10 @@ class _CatListState extends State<CatList> {
   }
 
   void _loadCats() async {
-    String fileData = await DefaultAssetBundle.of(context).loadString("assets/cats.json");
-    for (Cat c in CatApi.allCatsFromJson(fileData)) {
-      _cats.add(c);
-    }
-
-    print(_cats.toString());
+    String fileData =
+        await DefaultAssetBundle.of(context).loadString("assets/cats.json");
+    setState(() {
+      _cats = CatApi.allCatsFromJson(fileData);
+    });
   }
 }
