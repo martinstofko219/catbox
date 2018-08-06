65bb42827da182ef8717abdd1eda6e40327d9076
Martin Stofko <martinstofko114@gmail.com>
2018-08-05 18:31:54 -0700

    Fixed widget test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,26 +4,17 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:catbox/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App title smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new CatBoxApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our title is 'Cats'.
+    expect(find.text('Cats'), findsOneWidget);
+    expect(find.text('Dogs'), findsNothing);
   });
 }
