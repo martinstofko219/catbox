bf936b1d360d57009233722daff49c0faaba529e
Martin Stofko <martinstofko114@gmail.com>
2018-11-02 21:45:51 -0700

    Added cat details

--- lib/ui/cat_details/details_body.dart
+++ lib/ui/cat_details/details_body.dart
@@ -9,7 +9,76 @@ class CatDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    var theme = Theme.of(context);
+    var textTheme = theme.textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          cat.name,
+          style: textTheme.headline.copyWith(color: Colors.white),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 4.0),
+          child: _buildLocation(context),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: Text(
+            cat.description,
+            style: textTheme.body1.copyWith(
+              color: Colors.white70,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: Row(
+            children: <Widget>[
+              _buildCircleBadge(Icons.share, theme.accentColor),
+              _buildCircleBadge(Icons.phone, Colors.white12),
+              _buildCircleBadge(Icons.email, Colors.white12),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCircleBadge(IconData iconData, Color color) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: CircleAvatar(
+        backgroundColor: color,
+        radius: 16.0,
+        child: Icon(
+          iconData,
+          color: Colors.white,
+          size: 16.0,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLocation(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    return Row(
+      children: <Widget>[
+        Icon(
+          Icons.place,
+          color: Colors.white,
+          size: 16.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            cat.location,
+            style: textTheme.subhead.copyWith(color: Colors.white),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/ui/cat_details/details_page.dart
+++ lib/ui/cat_details/details_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
-import 'package:catbox/models/cat.dart';
-import 'package:catbox/ui/cat_details/header/details_header.dart';
+import '../../models/cat.dart';
+import './header/details_header.dart';
+import './details_body.dart';
 
 class CatDetailsPage extends StatefulWidget {
   final Cat cat;
@@ -37,7 +38,14 @@ class _CatDetailsPageState extends State<CatDetailsPage> {
           decoration: linearGradient,
           child: Column(
             children: <Widget>[
-              CatDetailsHeader(widget.cat, avatarTag: widget.avatarTag),
+              CatDetailsHeader(
+                widget.cat,
+                avatarTag: widget.avatarTag,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: CatDetailsBody(widget.cat),
+              ),
             ],
           ),
         ),
